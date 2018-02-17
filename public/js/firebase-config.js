c42b6e0cdfb1e25136eb488e2c7c106f5cb677a6
Kautuk Kundan <kautukkundan@gmail.com>
2018-02-17 17:28:50 +0530

    Delete firebase-config.js

--- public/js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyA4LBrYAzeqM-vhhGjJ4HCF8w6wKw6zeUY",
-    authDomain: "act-app-bc77b.firebaseapp.com",
-    databaseURL: "https://act-app-bc77b.firebaseio.com",
-    projectId: "act-app-bc77b",
-    storageBucket: "",
-    messagingSenderId: "126040849126"
-  };
-  firebase.initializeApp(config);
